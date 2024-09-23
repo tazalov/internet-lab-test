b4754309dc5244d4e6bdddbb876f2007a022559e
Ilya Tazalov <tazalov9@gmail.com>
2024-09-23 15:33:58 +0300

    config: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: '/internet-lab-test/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
