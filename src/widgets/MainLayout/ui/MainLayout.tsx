7ea19a1ae1ba5fe8dcb8d4eae379b495fd8ced92
Ilya Tazalov <tazalov9@gmail.com>
2024-09-23 15:19:07 +0300

    feat: add footer
    Config deploy to gh pages

--- src/widgets/MainLayout/ui/MainLayout.tsx
+++ src/widgets/MainLayout/ui/MainLayout.tsx
@@ -1,13 +1,21 @@
 import { PropsWithChildren } from 'react'
 
 import { routes } from '@/shared/const/routes'
+import { Typography } from '@/shared/ui/Typography'
 import { Navbar } from '@/widgets/Navbar'
+
+import s from './MainLayout.module.scss'
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Navbar items={routes} />
       {children}
+      <footer className={s.footer}>
+        <Typography variant='body18' className={s.copy}>
+          © 2021 Лаборатория интернет
+        </Typography>
+      </footer>
     </>
   )
 }
